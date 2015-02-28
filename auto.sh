655f8c9c04f618805c28f142fff14c786775faa1
walteryang <walteryang47@gmail.com>
2015-02-28 18:17:28 +0800

    image adjustments
    1. scale image to normal size
    2. remove redundant image description
    
    Signed-off-by: walteryang <walteryang47@gmail.com>

--- auto.sh
+++ auto.sh
@@ -16,6 +16,7 @@ find . -type f -print0 | xargs -0 sed -i 's/<li>//g'
 find . -type f -print0 | xargs -0 sed -i 's/<\/li>//g'
 find . -type f -print0 | xargs -0 sed -i 's/<li\/>//g'
 find . -type f -print0 | xargs -0 sed -i 's/..\/images\//images\//'
+find . -type f -print0 | xargs -0 sed -i '1N;$!N;s/\(!\[.*\](.*)\)[\n[ ]*]*\n[ ]*\*\*.*\*\*/\1/;P;D'
 python $BASEDIR/con.py ./SUMMARY.md docbook
 cd docbook
 find . -type f -print0 | xargs -0 sed -i 's/sect1/section/g'
@@ -24,6 +25,7 @@ find . -type f -print0 | xargs -0 sed -i '1N;$!N;s/<para>\n[ ]*<emphasis role="s
 find . -type f -print0 | xargs -0 sed -i '1N;$!N;s/<para>\n[ ]*<emphasis role="strong">概述<\/emphasis>\n[ ]*<\/para>/<formalpara><title>概述<\/title><para><\/para><\/formalpara>/;P;D'
 find . -type f -print0 | xargs -0 sed -i '1N;$!N;s/<para>\n[ ]*<emphasis role="strong">结果<\/emphasis>\n[ ]*<\/para>/<formalpara><title>结果<\/title><para><\/para><\/formalpara>/;P;D'
 find . -type f -execdir python $BASEDIR/xmlmod.py {} \;
+find . -type f -print0 | xargs -0 sed -i 's/\(<imagedata fileref="\)\(.*png" \)\(\/>\)/\1\2format="PNG" scale="50" \3/'
 
 cp -r ../../images/ ./zh-CN
 
